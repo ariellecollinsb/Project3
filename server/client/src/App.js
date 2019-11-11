30ce3e4acdc48296babd730ba70818075e3f1881
Arielle <ariellcollinsb@gmail.com>
2019-11-11 16:04:11 -0700

    underway again

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Search from "./pages/Search";
+import Discover from "./pages/Discover";
+import Home from "./pages/Home";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <NavBar/>
+     <Switch>
+       <Route exact path="/discover" component={Discover} />
+       <Route exact path="/search" component={Search} />
+       <Route component={Home} />
+     </Switch>
+    </Router>
   );
 }
 
---
+++ server/client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <section>
+            <h2>Home</h2>
+            <p>Everything you need to know 
+                about my meal prepper app!</p>
+        </section>
+
+    )
+}
+
+export default Home;
